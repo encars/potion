7558a23148834a4236d73f947d96b0a6ce294d68
Enric <enric.carstens@gmail.com>
2023-10-13 14:58:49 +0200

    initial setup

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./_components/navbar"
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="h-full">
+            <main className="h-full pt-40">
+                <Navbar />
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default MarketingLayout;
